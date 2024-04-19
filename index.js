6bdf008c59e94a45e5cfea92c5b7689d6eeb2d02
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-04-19 00:05:12 +0000

    PIRATAGE

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ╔════◇
-║ *『 WAOW YOU CHOOSE SUHAIL-MD 』*
-║ _You complete first step to making Bot._
+║ *『 BIENVENUE CHEZ HACKING-MD』*
+║ _besion d'aide contacter le support du numéro suivant._
 ╚════════════════════════╝
 ╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║ *Ytube:* _youtube.com/SuhailTechInfo_
-║ *Owner:* _https://wa.me/923184474176_
-║ *Note :*_Don't provide your SESSION_ID to_
-║ _anyone otherwise that can access chats_
+║  『••• AIDE HACKING-MD •••』
+║ *Ytube:* _https://youtube.com/@KouameDjakiss?si=k2HqPPSmHBZe3ABd_
+║ *Owner:* _https://wa.me/22588697148_
+║ *Note :*_Voici votre SESSION_ID chère ami_
+║ _HACKING-MD vous remercie et thomas_
 ╚════════════════════════╝
 `
 
